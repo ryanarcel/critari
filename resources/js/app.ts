876d054e1e70e5e634519d4999e566f571f66547
Ryan Arcel Galendez <ryanargee2@gmail.com>
2026-08-03 15:04:47 +0800

    chore: fix ziggy error

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -7,7 +7,7 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { createPinia } from 'pinia';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { ZiggyVue } from 'ziggy-js';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
