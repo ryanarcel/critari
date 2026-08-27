9d9ec29f86a2ab7f2405137580d5aee200c94f20
Ryan Arcel Galendez <ryanargee2@gmail.com>
2026-08-27 12:43:57 +0800

    feat: edit trust proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -22,6 +22,7 @@ class TrustProxies extends Middleware
     protected $headers =
         Request::HEADER_X_FORWARDED_FOR |
         Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
         Request::HEADER_X_FORWARDED_PROTO |
         Request::HEADER_X_FORWARDED_AWS_ELB;
 }
--- database/seeders/AppTenantSeeder.php
+++ database/seeders/AppTenantSeeder.php
@@ -11,13 +11,6 @@ class AppTenantSeeder extends Seeder
     {
         $tenant = Tenant::firstOrCreate(['id' => 'app']);
 
-        // Create domain for local development
         $tenant->domains()->firstOrCreate(['domain' => 'app']);
-
-        // Create domain for production if APP_DOMAIN is set and different
-        $appDomain = env('APP_DOMAIN', 'critari.com');
-        if ($appDomain !== 'critari.com') {
-            $tenant->domains()->firstOrCreate(['domain' => "app.{$appDomain}"]);
-        }
     }
 }
